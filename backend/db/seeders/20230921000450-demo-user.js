428c5428e2e26bf1682a3d3cdc631de56c477ae9
Shreya <shreya.r.darekar@gmail.com>
2023-09-21 17:20:49 -0700

    Refactor migration, model, seeder

--- backend/db/seeders/20230921000450-demo-user.js
+++ backend/db/seeders/20230921000450-demo-user.js
@@ -13,16 +13,22 @@ module.exports = {
     await User.bulkCreate(
       [
         {
+          firstName: "Demo",
+          lastName: "lition",
           email: "demo@user.io",
           username: "Demo-lition",
           hashedPassword: bcrypt.hashSync("password"),
         },
         {
+          firstName: "Demo1",
+          lastName: "lition1",
           email: "user1@user.io",
           username: "FakeUser1",
           hashedPassword: bcrypt.hashSync("password2"),
         },
         {
+          firstName: "Demo2",
+          lastName: "lition2",
           email: "user2@user.io",
           username: "FakeUser2",
           hashedPassword: bcrypt.hashSync("password3"),
