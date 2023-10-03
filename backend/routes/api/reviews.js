f37e8f0903a5fdade92c49c24b2363f57b014e99
Shreya <shreya.r.darekar@gmail.com>
2023-10-02 17:53:05 -0700

    Add an Image to a Review based on the Review's id

--- backend/routes/api/reviews.js
+++ backend/routes/api/reviews.js
@@ -61,4 +61,34 @@ router.get("/current", requireAuth, async (req, res) => {
   res.json({ Reviews: formattedReviews });
 });
 
+// Add an Image to a Review based on the Review's id
+router.post("/:reviewId/images", requireAuth, async (req, res) => {
+  const { user } = req;
+  const review = await Review.findByPk(req.params.reviewId, {
+    include: ReviewImage,
+  });
+
+  if (!review) {
+    return res.status(404).json({ message: "Review couldn't be found" });
+  }
+
+  if (review.userId !== user.id) {
+    return res
+      .status(404)
+      .json({ message: "Review must belong to the current user" });
+  }
+
+  if (review.ReviewImages.length >= 10) {
+    return res.status(403).json({
+      message: "Maximum number of images for this resource was reached",
+    });
+  }
+
+  const newReviewImage = await ReviewImage.create({
+    reviewId: review.id,
+    ...req.body,
+  });
+
+  res.json({ id: newReviewImage.id, url: newReviewImage.url });
+});
 module.exports = router;
